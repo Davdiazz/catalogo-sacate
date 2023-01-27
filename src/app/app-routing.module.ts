5cb724b5bd4aaafda875ecbaf0cbca2a936e21e4
Ivan Mauricio Diaz <idiaz@conexia.com>
2023-01-27 18:30:42 -0500

    Init: Catalogo module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: '',
+    redirectTo: 'catalogo',
+    pathMatch: 'full'
+  },
+  {
+    path: 'catalogo',
+    loadChildren: () => import(`./pages/catalogo/catalogo.module`).then(m => m.CatalogoModule),
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
